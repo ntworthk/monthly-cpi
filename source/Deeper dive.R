e4f45bb32cf35f890a4e039fc975e88fc6823252
Nick Twort <nicholas.twort@gmail.com>
2022-10-26 10:44:24 +1100

    Final before publishing

--- source/Deeper dive.R
+++ source/Deeper dive.R
@@ -12,8 +12,8 @@ library(directlabels)
 
 #--- Import data ---------------------------------------------------------------
 
-# monthly_cpi <- read_abs_local(filenames = "648401 (2).xlsx", path = "C:/Users/nick.twort/Downloads/")
-monthly_cpi <- read_abs(cat_no = "6484.0", tables = "1")
+monthly_cpi <- read_abs_local(filenames = "648401 (2).xlsx", path = "C:/Users/nick.twort/Downloads/")
+# monthly_cpi <- read_abs(cat_no = "6484.0", tables = "1")
 quarter_cpi <- read_abs_series("A2325846C") |> 
   mutate(series = "Index Numbers ;  All groups CPI (quarterly) ;  Australia ;")
 
@@ -28,49 +28,61 @@ monthly_cpi <- monthly_cpi |>
   )) |> 
   ungroup()
 
-monthly_cpi |>
-  filter(series_id %in% c(
-    "A2325846C", # quarterly all groups, original
-    "A128478317T", # all groups CPI, original
-    "A128473239F", # all groups CPI excluding volatile items, original
-    "A128481587A", # all groups CPI, seasonally adjusted
-    "A128481593W", # trimmed mean, seasonally adjusted
-    "A128485125F"  # weighted median, seasonally adjusted
-  )) |> 
-  filter(!is.na(value_ch), date >= "2018-09-01") |> 
-  mutate(series_2 = str_replace_all(series_2, c("CPI " = "CPI\n", "CPI, " = "CPI,\n"))) |> 
-  ggplot(
-    aes(x = date, y = value_ch, colour = series_2)
-  ) +
-  geom_line(size = 1) +
-  geom_text(
-    data = \(x) filter(group_by(x, series_2), date == max(date)),
-    aes(label = series_2),
-    hjust = -0.1
-    ) +
-  labs(
-    x = NULL,
-    y = "Annual CPI change (%)",
-    # colour = NULL,
-    # title = "Reaching an inflation peak?",
-    # subtitle = "ABS estimate 6.8% inflation in year to August, slightly less than the 7.0% to July",
-    caption = "Source: ABS, chart by Nick Twort"
-  ) +
-  scale_x_date(expand = expansion(mult = c(0.05, 0.15))) +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
-  scale_colour_manual(values = hktools::hk_colours, guide = guide_none()) +
-  theme_light(base_family = "Lato", base_size = 12) +
-  theme(
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    legend.background = element_blank(),
-    legend.key=element_blank(),
-    legend.position = c(0.85, 0.2),
-    strip.background = element_rect(fill = "#E6E7E8"),
-    plot.subtitle = element_text(size = 8)
-  )
 
-individual_chart <- function(series_id) {
+create_full_chart <- function(.data = monthly_cpi, title = NULL, subtitle = NULL, series = c("A2325846C", # quarterly all groups, original
+                                                "A128478317T", # all groups CPI, original
+                                                "A128473239F", # all groups CPI excluding volatile items, original
+                                                "A128481587A", # all groups CPI, seasonally adjusted
+                                                "A128481593W", # trimmed mean, seasonally adjusted
+                                                "A128485125F")) {  # weighted median, seasonally adjusted
+
+  .data |>
+    filter(series_id %in% {{series}}) |> 
+    filter(!is.na(value_ch), date >= "2018-09-01") |> 
+    mutate(series_2 = str_replace_all(series_2, c("CPI " = "CPI\n", "CPI, " = "CPI,\n"))) |> 
+    ggplot(
+      aes(x = date, y = value_ch, colour = series_2)
+    ) +
+    geom_line(size = 1) +
+    geom_point(
+      data = \(x) filter(group_by(x, series_2), date == max(date)),
+      size = 2
+    ) +
+    geom_text(
+      data = \(x) filter(group_by(x, series_2), date == max(date)),
+      aes(label = series_2),
+      hjust = -0.1
+    ) +
+    labs(
+      x = NULL,
+      y = "Annual CPI change (%)",
+      # colour = NULL,
+      title = title,
+      subtitle = subtitle,
+      caption = "Source: ABS, chart by Nick Twort"
+    ) +
+    scale_x_date(expand = expansion(mult = c(0.05, 0.25))) +
+    scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+    scale_colour_manual(values = hktools::hk_colours, guide = guide_none()) +
+    theme_light(base_family = "Lato", base_size = 12) +
+    theme(
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      legend.background = element_blank(),
+      legend.key=element_blank(),
+      legend.position = c(0.85, 0.2),
+      strip.background = element_rect(fill = "#E6E7E8"),
+      plot.subtitle = element_text(size = 8)
+    )
+  
+  
+}
+  
+  
+
+individual_chart <- function(series_id, colour_index = 1, title = FALSE, subtitle = FALSE) {
+  
+  
   
   chart_data <- monthly_cpi |> 
     filter(series_id == {{series_id}})
@@ -79,10 +91,16 @@ individual_chart <- function(series_id) {
   
   monthly_cpi |> 
     filter(series_id == {{series_id}}) |> 
+    filter(!is.na(value_ch)) |> 
     ggplot(
       aes(x = date, y = value_ch)
     ) +
-    geom_line(colour = "#008698", size = 1) +
+    geom_line(colour = hktools::hk_colours[colour_index], size = 1) +
+    geom_point(
+      data = \(x) filter(group_by(x, series_2), date == max(date)),
+      colour = hktools::hk_colours[colour_index],
+      size = 2
+    ) +
     geom_text(
       data = \(x) filter(group_by(x, series_2), date == max(date)),
       aes(label = scales::percent(value_ch, accuracy = 0.1)),
@@ -91,8 +109,8 @@ individual_chart <- function(series_id) {
     labs(
       x = NULL,
       y = "Annual CPI change (%)",
-      title = series_name,
-      subtitle = glue("ABS estimate {scales::percent(last(chart_data$value_ch), accuracy = 0.1)}"),
+      title = ifelse(title, series_name, ""),
+      subtitle = ifelse(subtitle, glue("ABS estimate {scales::percent(last(chart_data$value_ch), accuracy = 0.1)}"), ""),
       caption = "Source: ABS, chart by Nick Twort"
     ) +
     scale_x_date(expand = expansion(mult = c(0.05, 0.15))) +
@@ -112,18 +130,21 @@ individual_chart <- function(series_id) {
 }
 
 
-# Automotive fuel
-individual_chart("A128484685K")
+get_nth_change_yoy <- function(n = 1, .data = monthly_cpi) {
+  
+  .data |>
+    filter(!is.na(value_ch)) |>
+    filter(series_2 %in% (df |> distinct(group) |> pull(group))) |> 
+    filter(series_1 == "Index Numbers") |> 
+    arrange(desc(date), desc(value_ch)) |> 
+    head(n) |> 
+    tail(1) |> 
+    pull(series_id)
+  
+}
 
-# Food & non-alch
-individual_chart("A128480733V")
-
-# Fruit & veg
-individual_chart("A128480215V")
-
-# Rent
-individual_chart("A128473449C")
-
-# Housing
-individual_chart("A128485865L")
-
+lookup_series_name <- function(series_id, .data = monthly_cpi) {
+  
+  .data |> filter(series_id == {{series_id}}) |> head(1) |> pull(series_2)
+  
+}
